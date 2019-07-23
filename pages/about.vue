1de4f33cc73647633d4ea78d361271765a87dd10
Yaxue Guo <yguo@d2626uyguo1.aa.ad.epa.gov>
2019-07-23 16:56:22 -0400

    crash course start

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+	<h1>About DadJokes</h1>
+	<p>This is a demo app to exercise Nuxt js.</p>
+</div>
+</template>
+
+<script>
+export default {
+	head() {
+		return {
+			title: 'About this App',
+			meta: [
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'Best place for dad jokes'
+			}]
+		}
+	}
+}
+
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div>
+     <h2>Welcome to the dad joke app.</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+ export default {
+  head() {
+    return {
+      title: 'Welcome to the dad joke app.',
+      meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Best place for dad jokes'
+      }]
+    } 
+  }
+}
+</script>
